6dcee97efbfe85352f2603473b5162f480ac2af1
Wandell Rodrigues de Carvalho <Wandellcarvalho951108@gmail.com>
2022-03-30 21:48:12 -0300

    Projeto criado no visual studio 2019

---
+++ Temperatura.Spec/Temperatura.Spec/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,43 @@
+﻿using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace Temperatura.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        private double _temperaturaFahrenheit;
+        private double _temperaturaCelsius;
+        private double _temperaturaKelvin;
+
+        [Given("que o valor da temperatura e de (.*) graus Fahrenheit")]
+        public void PreencherTemperaturaFahrenheit(double valorFahrenheit)
+        {
+            _temperaturaFahrenheit = valorFahrenheit;
+        }
+
+        [When("eu solicitar a conversao desta teperatura")]
+        public void ProcessarConversao()
+        {
+            _temperaturaCelsius =
+                ConversorTemperatura.FahrenheitPraCelsius(
+                    _temperaturaFahrenheit);
+            _temperaturaKelvin =
+                ConversorTemperatura.FahrenheitPraKelvin(
+                    _temperaturaFahrenheit);
+        }
+
+        [Then("o resultado da conversao para Celsius sera de (.*) graus")]
+        public void ValidarResultadoCelsius(double valorCelsius)
+        {
+            _temperaturaCelsius.Should().Be(valorCelsius);
+        }
+
+        [Then("o resultado da conversao para Kelvin sera de (.*) graus")]
+        public void ValidarResultadoKelvin(double valorKelvin)
+        {
+            _temperaturaKelvin.Should().Be(valorKelvin);
+        }
+    }
+}
---
+++ Temperatura/Temperatura/ConversorTemperatura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Temperatura
+{
+    public static class ConversorTemperatura
+    {
+        public static double FahrenheitPraCelsius(double tempratura) {
+            //return (tempratura-32.0)/18.8;//Simulacao de falha
+            return Math.Round((tempratura-32.0)/1.8,2);
+        }
+        public static double FahrenheitPraKelvin(double tempratura) {
+            //return FahrenheitPraCelsius(tempratura) + 273.15;//Simulacao de falha
+            return Math.Round(FahrenheitPraCelsius(tempratura)+ 273.15,2);
+        }
+    }
+}
